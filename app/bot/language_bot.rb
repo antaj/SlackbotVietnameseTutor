2affc33cc285d53d01983bb0517e08b5a645f15b
anta-tinypulse <anta@tinypulse.com>
2016-01-14 20:05:03 +0700

    milestone 2

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,3 +1,7 @@
 class LanguageBot < SlackRubyBot::Bot
-
+  command 'hello' do |client, data, match|
+    I18n.locale = :vi
+    text = I18n.t "hello", user: data.user
+    client.say(text: text, channel: data.channel)
+  end
 end
