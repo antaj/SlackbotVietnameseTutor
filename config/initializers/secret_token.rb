d0c98d765c41823b7b9f001dad925ccebf7cb4ba
anta-tinypulse <anta@tinypulse.com>
2016-01-14 19:14:26 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlackbotVietnameseTutor::Application.config.secret_token = '532055f7775a74fc51c4dc11b4ac2335c009a871103a1d7a750cecf888c0991ebf860a823b487b315fa124a5cc8b54ab09cca5b09cb585249b7504f4cbb43d80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SlackbotVietnameseTutor::Application.config.session_store :cookie_store, key: '_SlackbotVietnameseTutor_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SlackbotVietnameseTutor::Application.config.session_store :active_record_store
